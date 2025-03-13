4b356e6d896912cf1230f89033744c6935301e78
Rafaella <fariassrafaella@gmail.com>
2025-03-12 22:21:58 -0300

    initial commit

---
+++ src/main/java/restiapi/restapiproj/domain/model/News.java
@@ -0,0 +1,8 @@
+package restiapi.restapiproj.domain.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_news")
+public class News extends BaseItem {
+
+}
---
+++ src/main/java/restiapi/restapiproj/domain/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package restiapi.restapiproj.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import restiapi.restapiproj.domain.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+//    boolean existsByAccountNumber(String number);
+}
